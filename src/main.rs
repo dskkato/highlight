e7451c22703b2ada35eda8868beae0c87245a3a0
Daisuke Kato <kato.daisuke429@gmail.com>
2019-04-14 12:14:26 +0900

    chech stream via isatty

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate atty;
+use atty::Stream;
+
+extern crate syntect;
 use syntect::dumps;
 use syntect::easy::HighlightLines;
 use syntect::highlighting::Style;
@@ -7,15 +11,19 @@ use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 fn main() {
     // Load these once at the start of your program
     let ps = SyntaxSet::load_defaults_newlines();
-    let th = dumps::from_binary(include_bytes!("../assets.bin"));
+    let th = dumps::from_binary(include_bytes!("../Monokai.bin"));
 
     let syntax = ps.find_syntax_by_extension("json").unwrap();
     let mut h = HighlightLines::new(syntax, &th);
     let s = r#"{"a":"b", "c":true, "d":[1,2,3]}"#;
     for line in LinesWithEndings::from(s) {
         // LinesWithEndings enables use of newlines mode
-        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
-        let escaped = as_24_bit_terminal_escaped(&ranges[..], false);
-        println!("{}", escaped);
+        if atty::is(Stream::Stdout) {
+            let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
+            let escaped = as_24_bit_terminal_escaped(&ranges[..], false);
+            println!("{}", escaped);
+        } else {
+            println!("{}", line);
+        }
     }
 }
